88af5d1c7ea944f275392ceeb17b0d7523f775f9
Juan Rossano <juanrossano@gmail.com>
2025-03-16 12:15:43 -0300

    Base inicial para el proyecto practico

---
+++ src/app/domains/products/components/product/product.component.ts
@@ -0,0 +1,21 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterLinkWithHref } from '@angular/router';
+import { Product } from '@shared/models/product.model';
+import { ReversePipe } from '@shared/pipes/reverse.pipe';
+import { TimeAgoPipe } from '@shared/pipes/time-ago.pipe';
+
+@Component({
+    selector: 'app-product',
+    imports: [CommonModule, ReversePipe, TimeAgoPipe, RouterLinkWithHref],
+    templateUrl: './product.component.html',
+})
+export class ProductComponent {
+  @Input({required: true}) product!: Product;
+
+  @Output() addToCart = new EventEmitter();
+
+  addToCartHandler() {
+    this.addToCart.emit(this.product);
+  }
+}
